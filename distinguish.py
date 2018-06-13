bf1a9d43e98251a3ff6e5eae399416b84f052f8e
Sebastian Weiser <weiser.sebastian@gmx.de>
2018-06-13 11:36:47 +0200

    restructured packages

--- distinguish.py
+++ distinguish.py
@@ -5,10 +5,13 @@ from random import shuffle
 import numpy as np
 from PIL import Image
 
+from models.vanilla_conv import VanillaConv
+
 base_dir = "D:\\noise\\"
 number_of_labels = 1127
 batch_size = 16
-learning_rate = 1e-3
+
+model = VanillaConv()
 
 def load_data():
     with open(base_dir + "records\\1ff235e4-01e8-469f-a8af-87395bfd7f0d_cut.txt") as f:
@@ -37,48 +40,6 @@ def create_batch(data):
         yield data[on:off]
 
 
-inputs = tf.placeholder(tf.float32, [None, 513, 469, 1])
-labels = tf.placeholder(tf.int32, [None])
-dropout_rate = tf.placeholder(tf.float32)
-
-
-conv_0_1 = tf.layers.conv2d(inputs, 32, 3)
-pool_0 = tf.layers.max_pooling2d(conv_0_1, [3, 3], 2)
-
-conv_1_1 = tf.layers.conv2d(pool_0, 64, 3)
-conv_1_2 = tf.layers.conv2d(conv_1_1, 64, 3)
-conv_1_3 = tf.layers.conv2d(conv_1_2, 64, 3)
-pool_1 = tf.layers.max_pooling2d(conv_1_3, [3, 3], 2)
-conv_2_1 = tf.layers.conv2d(pool_1, 64, 3)
-conv_2_2 = tf.layers.conv2d(conv_2_1, 64, 3)
-conv_2_3 = tf.layers.conv2d(conv_2_2, 64, 3)
-pool_2 = tf.layers.max_pooling2d(conv_2_3, [3, 3], 2)
-conv_3_1 = tf.layers.conv2d(pool_2, 128, 3)
-conv_3_2 = tf.layers.conv2d(conv_3_1, 128, 3)
-conv_3_3 = tf.layers.conv2d(conv_3_2, 128, 3)
-pool_3 = tf.layers.max_pooling2d(conv_3_3, [3, 3], 2)
-conv_4_1 = tf.layers.conv2d(pool_3, 64, 3)
-conv_4_2 = tf.layers.conv2d(conv_4_1, 32, 3)
-conv_4_3 = tf.layers.conv2d(conv_4_2, 32, 3)
-pool_4 = tf.layers.max_pooling2d(conv_4_3, 3, 2)
-conv_5_1 = tf.layers.conv2d(pool_4, 32, 3)
-conv_5_2 = tf.layers.conv2d(conv_5_1, 32, 3)
-
-shape = conv_5_2.get_shape()
-print(shape)
-shape = [tf.shape(conv_5_2)[0], shape[1] * shape[2] * shape[3]]
-
-dropout = tf.layers.dropout(tf.reshape(conv_5_2, shape), rate=dropout_rate)
-readout_0 = tf.layers.dense(dropout, 1024)
-dropout_2 = tf.layers.dropout(readout_0, rate=dropout_rate)
-readout = tf.layers.dense(dropout_2, 2)
-
-loss = tf.losses.sparse_softmax_cross_entropy(labels, readout)
-optimize = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(loss)
-
-prediction_comparison = tf.equal(tf.cast(labels, tf.int64), tf.argmax(readout, axis=1))
-accuracy = tf.reduce_mean(tf.cast(prediction_comparison, tf.float32))
-
 def load_image(file):
     return np.reshape(list(Image.open(base_dir + "spectograms\\5s\\" + file).getdata()), [513, 469, 1]).astype('uint8')
 
@@ -91,8 +52,8 @@ def check_accuracy(sess, test_data):
         tags = unzipped[0]
         images = [load_image(img) for img in unzipped[1]]
 
-        _accuracy, _prediction = sess.run([accuracy, prediction_comparison],
-                                          {inputs: images, labels: tags, dropout_rate: 0})
+        _accuracy, _prediction = sess.run([model.accuracy, model.prediction_comparison],
+                                          {model.inputs: images, model.labels: tags, model.dropout_rate: 0})
 
         errors.extend([(unzipped[0][idx], unzipped[1][idx]) for idx, val in enumerate(_prediction) if val == False])
 
@@ -122,8 +83,8 @@ def train_net(test_data, training_data):
                 tags = unzipped[0]
                 images = [load_image(img) for img in unzipped[1]]
 
-                _, _loss = sess.run([optimize, loss], {inputs: images, labels: tags,
-                                                       dropout_rate: 0.3})
+                _, _loss = sess.run([model.optimize, model.loss], {model.inputs: images, model.labels: tags,
+                                                       model.dropout_rate: 0.3})
                 losses.append(_loss)
 
             print('Losses:', losses)
@@ -142,7 +103,7 @@ def train_net(test_data, training_data):
 def load_checkpoint(sess):
     saver = tf.train.Saver()
     saver.restore(sess, 'D:\\noise\\checkpoints\\test')
-    return readout, inputs, dropout_rate
+    return model.readout, model.inputs, model.dropout_rate
 
 
 if __name__ == '__main__':
